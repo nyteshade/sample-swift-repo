f314b89cabed39e3fc4a126aed0d94e51058a8db
Brielle Harrison <nyteshade@gmail.com>
2019-03-06 21:21:49 -0800

    Initial Commit

---
+++ Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Brielle Harrison on 3/6/19.
+//  Copyright © 2019 Nyteshade Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
